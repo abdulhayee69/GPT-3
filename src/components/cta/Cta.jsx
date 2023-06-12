a2aa53e04285a35bb5f28b61e553bbb0ff1b1671
Abdul Hayee <Abdulhayeebaig559@gmail.com>
2023-06-12 15:36:10 +0500

    Components created

---
+++ src/components/cta/Cta.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './cta.css';
+
+function Cta() {
+  return (
+    <div>
+      CTA
+    </div>
+  )
+}
+
+export default Cta
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
