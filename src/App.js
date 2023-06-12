51f4aa0bb7db71d70b5908f2d6905e5db4f884f8
Abdul Hayee <Abdulhayeebaig559@gmail.com>
2023-06-12 18:40:14 +0500

    Code Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Brand, CTA, Navbar } from './components';
-import { Footer, Blog, Possibility, Features, WhatGPT69, Header } from './containers'; 
-import './App.css';
-const App = () => {
-  return (
-    <div className='App'>
-      <div className='gradient__bg'>
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT69 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
-    </div>
-  )
-}
 
-export default App
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
+
+export default App;
